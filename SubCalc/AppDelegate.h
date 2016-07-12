3f0578e30b47e99369f8a768dbb8685c1a3f8139
Asuk Nath <nath@macpros01.local>
2016-07-12 14:23:33 -0400

    Initial Commit

---
+++ SubCalc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SubCalc
+//
+//  Created by Asuk Nath on 7/12/16.
+//  Copyright © 2016 Asuk Nath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
